70975731f4e192c926edaafeeaec57890b2a93c5
BilalCoding <88141297+BilalCoding@users.noreply.github.com>
2023-08-25 11:39:44 +0100

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,28 @@
+# NOTE: The video created does not run on VSCode, so you have to run it on a different app e.g. Media Player
+
+import os
+import cv2
+
+path = "C:/Users/bilal/Desktop/Coding Files/Projects/105/Images"
+images = []
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+    if ext in [".png", ".jpg", ".gif", ".jpeg", ".jfif"]:
+        file_name = path + "/" + file
+        print(file_name)
+        images.append(file_name)
+
+count = len(images)
+frame = cv2.imread(images[0])
+height, width, channels = frame.shape
+size = (width, height)
+print(size)
+out = cv2.VideoWriter("Project.mp4", cv2.VideoWriter_fourcc(*'DIVX'), 0.8, size)
+
+for i in range(0, count-1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+
+out.release()
+print("Done")
